0222b47d5685e922f03b3578f123fbefcae29678
heesa <heesang0224@gmail.com>
2026-02-10 00:07:44 +0900

    first

--- src/main/kotlin/org/pl/practice/GoodCommand.kt
+++ src/main/kotlin/org/pl/practice/GoodCommand.kt
@@ -1,8 +1,11 @@
 package org.pl.practice
 
+import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 class GoodCommand : CommandExecutor {
     override fun onCommand(
@@ -11,7 +14,22 @@ class GoodCommand : CommandExecutor {
         label: String,
         args: Array<out String>
     ): Boolean {
-        sender.sendMessage("Good command")
+
+        when (command.name.lowercase()) {
+            "good" -> {
+                if (sender !is Player) return true
+                sender.inventory.addItem(ItemStack(Material.GOLD_INGOT))
+            }
+
+
+
+
+
+
+
+
+            "help" -> sender.sendMessage("Help command")
+        }
         return true
     }
 }
--- src/main/kotlin/org/pl/practice/Practice.kt
+++ src/main/kotlin/org/pl/practice/Practice.kt
@@ -1,21 +1,34 @@
 package org.pl.practice
 
+import org.bukkit.entity.Creeper
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.ExplosionPrimeEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Practice : JavaPlugin(), Listener {
+    private lateinit var abilityManager: AbilityManager
+
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
-        logger.info("Enabling plugin")
+        logger.info("Plugin enabled")
+
+        abilityManager = AbilityManager(this)
+        abilityManager.register()
 
         getCommand("good")?.setExecutor(GoodCommand())
-
+        getCommand("help")?.setExecutor(GoodCommand())
+        getCommand("ability")?.setExecutor { sender, _, _, _ -> abilityManager.handleAbilityCommand(sender) }
+        getCommand("abilitycancel")?.setExecutor { sender, _, _, _ -> abilityManager.handleAbilityCancelCommand(sender) }
     }
 
+    @EventHandler
+    fun onExplosionPrime(event: ExplosionPrimeEvent) {
+        val creeper = event.entity as? Creeper ?: return
+
+        val base = event.radius
+        val power = base * 20.0f
+
+        event.radius = power.coerceAtMost(12.0f)
+    }
 }
-
-
-
-
-
-
